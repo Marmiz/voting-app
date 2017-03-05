a0b70f83c86cec502d229fc18ce9f43b5752b0a5
Claudio Restifo <claudio@samuiweb.net>
2017-03-05 20:46:13 +0700

    next test

--- src/Test/Core.test.js
+++ src/Test/Core.test.js
@@ -1,9 +1,9 @@
+/*jshint esversion: 6 */
 /*
 * Test if the application correctly load in a set of entries
 */
-
 import {List, Map} from 'immutable';
-import {setEntries} from '../core'; //takes a previous state and include them.
+import {setEntries, next} from '../core'; //takes a previous state and include them.
 
 describe('app logic', () => {
 
@@ -26,5 +26,25 @@ describe('app logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
-  })
+  });
+
+  /* The two first entries are included under the key pair.
+  The entries under vote should no longer be in the entries List */
+  describe('next', () =>{
+
+    it('take the next two entries under vote', () =>{
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).toEqual(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }).toJS());
+    });
+
+  });
+
 });
--- src/core.js
+++ src/core.js
@@ -1,9 +1,18 @@
+/*jshint esversion: 6 */
 /*
 * setEntries can just set an entries key in the state Map
 * and set the value as the given List of entries.
 */
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
+}
+
+export function next(state){
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  }).toJS();
 }
