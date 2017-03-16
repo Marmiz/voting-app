c22b7b692338e1dc6b461ea86306b053408773d8
Marmiz <claudio.restifo@gmail.com>
2017-03-16 22:55:17 +0700

    init react component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  getPair(){
+    return this.props.pair || [];
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +14,11 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        {this.getPair().map(entry =>
+          <button key={entry}>
+          <h1>{entry}</h1>
+          </button>
+        )}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import './index.css';
 import startServer from './server';
 import makeStore from './store';
 
-export const store = makeStore();
-startServer(store);
+// export const store = makeStore();
+// startServer(store);
+
+const pair = ['Trainspotting', '28 Days Later'];
 
 // get a starting point for our app.
-store.dispatch({
-  type: 'SET_ENTRIES',
-  entries: require('./entries.json')
-});
-store.dispatch({type: 'NEXT'});
+// store.dispatch({
+//   type: 'SET_ENTRIES',
+//   entries: require('./entries.json')
+// });
+// store.dispatch({type: 'NEXT'});
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <App pair={pair}/>,
+  document.getElementById('root')
+);
